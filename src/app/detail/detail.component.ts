d415caa3adc19a2e35a64e8f2527d376968db955
koustubh89 <koustubh89@github.com>
2017-09-05 14:59:49 +0530

    fetching route params in the child controlller

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { HackerNewsService } from '../shared/hacker-news.service';
 import { Post } from '../shared/post.interface';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-hn-detail',
@@ -10,11 +12,24 @@ import { Post } from '../shared/post.interface';
   styleUrls: ['./detail.component.css'],
   providers: [HackerNewsService]
 })
-export class AppDetailComponent {
-  posts: Post[];
+export class AppDetailComponent implements OnInit {
 
-  constructor(private _hackerNewsService: HackerNewsService) {
+  private sub: any;
+  posts: Post[];
+  id: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private _hackerNewsService: HackerNewsService,
+  ) {
     console.log('reaching app details component');
+    console.log('constructor => this.id', this.id);
   }
 
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.id = +params['id']; // (+) converts string 'id' to a number
+       console.log('onInit => this.id', this.id);
+    });
+  }
 }
